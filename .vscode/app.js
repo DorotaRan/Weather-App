42b9136182b4ae0df4198fc2fbcab130804a0c95
Dorota Rankowska <dorota.rankowska@gmail.com>
2020-11-19 22:37:13 +0100

    html and css initial structur

---
+++ .vscode/app.js
@@ -0,0 +1,23 @@
+const apiKey = ab06cf297e8f65ab0d86ffab4a9e88b4;
+const inputVal = Warsaw,pl;
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputVal}&appid=${apiKey}&units=metric`;
+
+class Data {
+    async getData(){
+        fetch(url)
+        .then(response => response.json())
+        console.log(response)
+        .then(data => {
+            
+        
+        })
+        .catch(() => {
+          alert("invalid location");
+        });
+    }
+};
+
+
+document.addEventListener('DOMContentLoaded' , () => {
+    const data = new Data()
+})
